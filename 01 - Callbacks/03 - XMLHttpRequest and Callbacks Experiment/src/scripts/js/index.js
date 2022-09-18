a61291c479da8b63090790ff162bce26bb82ccb6
Luis Marquez <englamc@gmail.com>
2022-09-18 18:24:16 -0400

    To solve

--- 01 - Callbacks/03 - XMLHttpRequest and Callbacks Experiment/src/scripts/js/index.js
+++ 01 - Callbacks/03 - XMLHttpRequest and Callbacks Experiment/src/scripts/js/index.js
@@ -1,6 +1,22 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
+function changeImage(route) {
+    document.getElementById('card--image').innerHTML = '<img src="`${route}`"/>';
+}
+
+function changeName(texto) {
+    document.getElementById('card--name').innerHTML = '<h3>`${texto}`</h3>';
+}
+
+function changeCategorie(texto) {
+    document.getElementById('card--categorie').innerHTML = '<h4>`${texto}`</h4>';
+}
+
+function changeDescription(texto) {
+    document.getElementById('card--description').innerHTML = '<p>`${texto}`</p>';
+}
+
 function fetchApi(urlApi, callback) {
 
     // Creating the request object - class XMLHttpRequest
@@ -44,27 +60,35 @@ function fetchApi(urlApi, callback) {
     request.send();
 }
 
-fetchApi(`${API}/products`, function (error1, data1) {
-    if(error1) {
-        return console.error(error1);
-    }
-    else {
-        fetchApi(`${API}/products/${data1[0].id}`, function (error2, data2) {
-            if(error2) {
-                return console.error(error2);
-            }
-            else {
-                fetchApi(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
-                    if(error3) {
-                        return console.error(error3);
-                    }
-                    else {
-                        console.log(data1[0]);
-                        console.log(data2.title);
-                        console.log(data3.name);
-                    }
-                });
-            }
-        });
-    }
-});
+function changeCardValues() {
+    let pid = document.getElementById('Id_number').value;
+
+    fetchApi(`${API}/products`, pid,function (error1, data1) {
+        if(error1) {
+            return console.error(error1);
+        }
+        else {
+            fetchApi(`${API}/products/${data1[pid].id}`, function (error2, data2) {
+                if(error2) {
+                    return console.error(error2);
+                }
+                else {
+                    fetchApi(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+                        if(error3) {
+                            return console.error(error3);
+                        }
+                        else {
+                            console.log(data1[pid]);
+                            console.log(data2.title);
+                            changeName(data2.title)
+                            console.log(data3.name);
+                            changeCategorie(data3.name);
+                            console.log(data3.image);
+                            changeImage(data3.image)
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
