28ace328a5ab260fdc886c07d4fa9f5a9b529949
Luis Marquez <englamc@gmail.com>
2022-09-20 20:01:39 -0400

    Promises - FetchData

---
+++ 02 - Promises/02 - Fetch using Promises/src/scripts/js/index.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch';
+
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi) {
+    return fetch(urlApi);
+}
+
+// fetchData(`${API}/products`)
+// .then(response => response.json())
+// .then(products => {
+//     console.log(products);
+// })
+// .then(() => {
+//     console.log("Hello!")
+// }).catch(
+//     error => console.log(error)
+// );
+
+fetchData(`${API}/products`)
+.then(response => response.json())
+.then(products => {
+    console.log(products)
+    return fetchData(`${API}/products/${products[0].id}`)
+})
+.then(response => response.json())
+.then(product => {
+    console.log(product.title)
+    return fetchData(`${API}/categories/${product.category.id}`)
+})
+.then(response => response.json())
+.then(category => {
+    console.log(category.name);
+})
+.catch(error => {console.error("ERROR: " + error)})
+.finally(() => console.log("Finally"));
