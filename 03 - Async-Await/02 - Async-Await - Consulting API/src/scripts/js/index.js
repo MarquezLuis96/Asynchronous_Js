79f3e747625bdb1a5ea6bbb1efffa3162a70a81b
Luis Marquez <englamc@gmail.com>
2022-09-26 16:58:57 -0400

    Async-Await - Fetching an API using Async-Await

---
+++ 03 - Async-Await/02 - Async-Await - Consulting API/src/scripts/js/index.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+const API = "https://api.escuelajs.co/api/v1";
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data;
+}
+
+const anotherFunction = async (urlApi) => {
+    try {
+        const products = await fetchData(`${API}/products`);
+        const product = await fetchData(`${API}/products/${products[0].id}`);
+        const category = await fetchData(`${API}/categories/${product.category.id}`);
+
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+    }
+    catch (error) {
+        console.error("Error: " + error);
+    }
+}
+
+anotherFunction(API);
